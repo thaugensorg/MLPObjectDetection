818c3fd4a64ce182ef1964749322ca0a5132f704
toddhaugen <todd@haugen.name>
2019-09-30 17:20:26 -0700

    work on image dimensions

--- EvaluateData/__init__.py
+++ EvaluateData/__init__.py
@@ -19,10 +19,10 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
                 status_code=400
         )
 
-    labeled_regions_json = req.params.get('LabeledRegions')
-    if not labeled_regions_json:
+    image_labeling_json = req.params.get('LabeledRegions')
+    if not image_labeling_json:
         try:
-            labeled_regions_json = req.get_json()
+            image_labeling_json = req.get_json()
         except:
             return func.HttpResponse(
                 "Please pass JSON containing the labeled regions associated with this image on the query string or in the request body.",
@@ -38,22 +38,41 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     training_key = os.environ['TrainingKey']
 
     # load labeled image regions passed in request into dictionary
-    labeled_regions = json.loads(labeled_regions_json)
+    image_labeling_data = json.loads(image_labeling_json)
 
     # instanciate custom vision client
     trainer = CustomVisionTrainingClient(training_key, endpoint=endpoint)
 
     # get list of valid tags for this model
     tags = trainer.get_tags(project_id)
-    for labeled_region in labeled_regions:
-        for label in labeled_region:
+
+    # get the height and width of the image
+    image_width = image_labeling_data['asset']['size']['width']
+    image_height = image_labeling_data['asset']['size']['height']
+
+    # for each labeled region in this asset get the map lables to tag ids and map boundaries formats
+    # from vott to azure cognitive services then upload to the cognitive services project.
+    for labeled_region in image_labeling_data['regions']:
+        for label in labeled_region['tags']:
             for tag in tags:
                 if tag.name == label:
-                    Labels.append(Tag.id)
-                    CountOfTagsAppliedToTimage = CountOfTagsAppliedToTimage + 1
+                    labels.append(tag.id)
+                    count_of_tags_applied_to_image = count_of_tags_applied_to_image + 1
                     break
+        top_left_x = labeled_region['points'][0]['x']
+        top_left_y = labeled_region['points'][0]['y']
+        top_right_x = labeled_region['points'][1]['x']
+        top_right_y = labeled_region['points'][1]['y']
+        bottom_right_x = labeled_region['points'][2]['x']
+        bottom_right_y = labeled_region['points'][2]['y']
+        bottom_left_x = labeled_region['points'][3]['x']
+        bottom_left_y = labeled_region['points'][3]['y']
 
-
+        # calculate normalized coordinates.
+        normalized_left = top_left_x / image_width
+        normalized_top = top_left_y / image_height
+        normalized_width = (top_right_x - top_left_x) / image_height
+        normalized_height = (bottom_left_y - top_left_y) / image_height
 
     if name:
         return func.HttpResponse(f"Hello {name}!")
