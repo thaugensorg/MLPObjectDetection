868483f3c07c76c96176bd351c80a0829bd3527e
toddhaugen <todd@haugen.name>
2019-10-23 17:41:37 -0700

    Enable data URL to be passed in the body of the request

--- EvaluateData/__init__.py
+++ EvaluateData/__init__.py
@@ -2,6 +2,7 @@ import logging
 import os
 import json
 import requests
+import copy
 
 import azure.functions as func
 
@@ -17,11 +18,15 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     data_url = req.params.get('ImageUrl')
     if not data_url:
         try:
-            req_body = req.get_json()
+            req_body = req.get_body()
         except ValueError:
-            pass
+            return func.HttpResponse(
+                "Please pass a ImageUrl on the query string or in the request body",
+                status_code=412
+            )
+
         else:
-            data_url = req_body.get('ImageUrl')
+            data_url = req_body.decode("utf-8")
 
     if data_url:
 
@@ -36,26 +41,29 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         iterations = trainer.get_iterations(project_id)
         if len(iterations) != 0:
 
+            # get the name of the current published iteration as that is required in the url
             current_iteration = iterations[0]
             current_iteration_name = current_iteration.publish_name
 
+            # format the url to call the custom vision model
             http_endpoint = client_endpoint + "customvision/v3.0/Prediction/" + project_id + "/detect/iterations/" + current_iteration_name + "/url"
 
+            # add headers and body to the call and get the response
             headers = {'Prediction-Key': prediction_key, 'Content-Type': 'application/json'}
             data = {"url": data_url}
             response = requests.post(http_endpoint, headers = headers,
                                     json = data)
 
+            # format the response to include the required json name 'confidence'
             response_dictionary = response.json()
             prediction = response_dictionary['predictions'][0]
             confidence = prediction['probability']
             response_dictionary['confidence'] = confidence
-                
-            # Display the results.
+    
+            # return the json results of the object detection custom vision model.
             return func.HttpResponse(json.dumps(response_dictionary))
 
         else:
-            # return f'Model not trained.'
             return func.HttpResponse(
                 "Model not trained.",
                 status_code=409
